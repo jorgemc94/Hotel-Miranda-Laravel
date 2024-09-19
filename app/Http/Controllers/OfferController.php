acbd58cc88395950503ecf5b9225accd8121e037
jorgemc94 <jmaciasc1994@gmail.com>
2024-09-19 23:37:19 +0200

    modify view offer

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
 use Illuminate\Http\Request;
 
 class OfferController extends Controller
 {
     public function index()
     {
-        return view('miranda.offer');
+        $rooms = Room::with(['bookings', 'amenities', 'photos'])->get();
+        return view('miranda.offer', compact('rooms'));
+    }
+
+    public function show(string $id)
+    {
+        $rooms = Room::with(['bookings', 'amenities', 'photos'])->get();
+        $room = Room::with(['bookings', 'amenities', 'photos'])->findOrFail($id);
+        return view('miranda.rooms.room-details', ['rooms' => $rooms , 'room' => $room]);
     }
 }
